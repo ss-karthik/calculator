b8a49e00ff0ed5db05f21897367afe59d64e4ab2
ss-karthik <maniac.sskarthik@gmail.com>
2023-10-11 05:15:55 +0530

    Add keyboard support

--- script.js
+++ script.js
@@ -48,13 +48,116 @@ buttons.addEventListener("click", function(event) {
     }
 });
 
+document.addEventListener("keydown", function(event) {
+    if (event.key == '/') {
+        event.preventDefault();
+    }
+    switcher(event.key);
+});
+
+function switcher(key) {
+    switch(key) {
+        case "1":
+            display.textContent += '1';
+        break;
+
+        case "2":
+            display.textContent += '2';
+        break;
+
+        case "3":
+            display.textContent += '3';
+            break;
+        case "4":
+            display.textContent += '4';
+            break;
+        case "5":
+            display.textContent += '5';
+            break;
+        case "6":
+            display.textContent += '6';
+            break;
+        case "7":
+            display.textContent += '7';
+            break;
+        case "8":
+            display.textContent += '8';
+            break;
+        case "9":
+            display.textContent += '9';
+            break;
+        case "0":
+            display.textContent += '0';
+            break;
+        case "+":
+            if(!operatorPressed) {
+                operatorPressed = true;
+                display.textContent += ' + ';
+            }
+            else if (operatorPressed) {
+                evaluate();
+                resultDisplayed = false;
+                display.textContent += ' + ';
+            }
+            break;
+        case "-":
+            if(!operatorPressed) {
+                operatorPressed = true;
+                display.textContent += ' - ';
+            }
+            else if (operatorPressed) {
+                evaluate();
+                resultDisplayed = false;
+                display.textContent += ' - ';
+            }
+            break;
+        case "*":
+            if(!operatorPressed) {
+                operatorPressed = true;
+                display.textContent += ' * ';
+            }
+            else if (operatorPressed) {
+                evaluate();
+                resultDisplayed = false;
+                display.textContent += ' * ';
+            }
+            break;
+        case "/":
+            
+            if(!operatorPressed) {
+                operatorPressed = true;
+                display.textContent += ' / ';
+            }
+            else if (operatorPressed) {
+                evaluate();
+                resultDisplayed = false;
+                display.textContent += ' / ';
+            }
+            break;
+        case ".":
+            if (!decimalDisplayed) {
+                display.textContent += '.';
+                decimalDisplayed = true;
+            }
+            break;
+        case "Enter":
+            evaluate();
+            break;
+        case "Backspace":
+            display.textContent = display.textContent.slice(0,-1);
+            break;
+        case "Space":
+            defaults();
+    }
+}
+
 function evaluate() {
     let string = display.textContent;
     const arr = string.split(" ");
     if(arr.length === 3) {
-        let num1 = parseInt(arr[0]);
+        let num1 = parseFloat(arr[0]);
         let opp = arr[1];
-        let num2 = parseInt(arr[2]);
+        let num2 = parseFloat(arr[2]);
         const result = operate(num1, num2, opp);
         if (isNaN(result)) {
             display.textContent = "Invalid Input";
